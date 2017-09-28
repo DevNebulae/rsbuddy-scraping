5956b196c3f0f3707e34e4a5b3963f9606c1c263
DevNebulae <gamerkeeperson+github@gmail.com>
2017-09-28 22:44:29 +0200

    Change Retriever class to  a function called "retriever"

--- retriever.py
+++ retriever.py
@@ -3,24 +3,19 @@ Class module which is meant to asynchronously retrieve all
 JSON data from the RSBuddy API.
 '''
 
-from threading import Thread
+import json
+from fs_utils import write_file
 from scraper_utils import scrape
 
-class Retriever(Thread):
-	'''
-	Class meant as an asynchronous getter for the RSBuddy
-	API JSON data. It needs a list of item ids which it will
-	fetch, the timestamp (in milliseconds) to where it will
-	retrieve the data and the scraper instance which it will
-	call.
-	'''
-    def __init__(self, item_ids, start, scraper):
-        self.item_ids = item_ids
-        self.start = start
-        self.scraper = scraper
-
-    def run(self):
-        for identifier in self.item_ids:
-            url = f'https://api.rsbuddy.com/grandExchange?a=graph&g=30&start={self.start}&i={identifier}'
-            data = scrape(self.scraper, url)
-            print(f'{identifier} done')
+def retriever(item_ids, start, scraper):
+    '''
+    Function meant as an asynchronous getter for the RSBuddy
+    API JSON data. It needs a list of item ids which it will
+    fetch, the timestamp (in milliseconds) to where it will
+    retrieve the data and the scraper instance which it will
+    call.
+    '''
+    for identifier in item_ids:
+        url = f'https://api.rsbuddy.com/grandExchange?a=graph&g=30&start={start}&i={identifier}'
+        data = scrape(scraper, url)
+        write_file(f'.data/{identifier}.json', json.dumps(data))
