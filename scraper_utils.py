6e7e743bf193fc175b485838fb5a7c182dd1b794
DevNebulae <gamerkeeperson+github@gmail.com>
2017-09-28 22:43:52 +0200

    Update scraper_utils python module with cached function and docs

--- scraper_utils.py
+++ scraper_utils.py
@@ -3,9 +3,19 @@ General utility module which contains generalized code to
 retrieve data.
 '''
 
+import json
+import os
+
+def cached(scraper, path, url):
+    if os.path.isfile(path):
+        with open(path, 'r') as _file:
+            return json.load(_file)
+    else:
+        return scrape(scraper, url)
+
 def scrape(scraper, url):
-	'''
-	Utility function which retrieves data from the specified
-	url and decodes it in the latin-1 format.
-	'''
-    return scraper.get('https://rsbuddy.com/exchange/names.json').content.decode('latin1')
+    '''
+    Utility function which retrieves data from the specified
+    url and decodes it in the latin-1 format.
+    '''
+    return json.loads(scraper.get(url).content.decode('latin1'))
