d7c2be3958eb9034603c1d5f45750fed6127e51b
DevNebulae <gamerkeeperson+github@gmail.com>
2017-10-01 16:39:49 +0200

    Clean up logging, add more logging and wait for threads to join

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ from random import shuffle
 import cfscrape
 from fs_utils import touch_dir, write_file
 import numpy as np
-from retriever import retriever
+from retriever import Retriever
 from scraper_utils import cached
 
 def main():
@@ -23,7 +23,7 @@ def main():
 
     logger.addHandler(file_handler)
 
-    logger.log(logging.INFO, 'Start downloading items')
+    logger.info('Started downloading items.')
 
     scraper = cfscrape.create_scraper()
     items = cached(scraper, '.data/items.json', 'https://rsbuddy.com/exchange/names.json')
@@ -31,24 +31,31 @@ def main():
     # Write caching file to .data folder
     write_file('.data/items.json', json.dumps(items))
 
+    logger.debug('Finished downloading items.')
+
     # Divide the items into buckets for the retrievers to
     # process
     # TODO: Add command line options for these variables
-    threads = 4
-    start = 1420070400000
+    thread_amount = 4
+    timestamp = 1420070400000
     max_retries = 5
 
+    threads = []
     item_ids = list(items.keys())[:100]
     shuffle(item_ids)
-    item_id_buckets = np.array_split(item_ids, threads)
+    item_id_buckets = np.array_split(item_ids, thread_amount)
 
-    logger.log(logging.INFO, 'Start downloading item history')
+    logger.info('Start downloading item history')
 
     # Create all retrievers and run them as a seperate
     # thread
     for item_id_bucket in item_id_buckets:
-        thread = Thread(target=retriever, args=(item_id_bucket, start, scraper, max_retries, logger))
+        thread = Retriever(item_id_bucket, timestamp, scraper, max_retries, logger)
         thread.start()
+        threads.append(thread)
+
+    for thread in threads:
+        thread.join()
 
 if __name__ == "__main__":
     main()
