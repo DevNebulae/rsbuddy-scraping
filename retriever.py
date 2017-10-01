186ed17794b698723da405aff79c2aa73e0968b1
DevNebulae <gamerkeeperson+github@gmail.com>
2017-10-01 16:38:51 +0200

    Transform retriever to class structure

--- retriever.py
+++ retriever.py
@@ -4,39 +4,52 @@ JSON data from the RSBuddy API.
 '''
 
 import json
-import logging
 import time
+from threading import Thread
 from fs_utils import write_file
 from scraper_utils import scrape
 
-def retrieve(item_id, start, scraper, max_retries, retries, logger):
-    url = f'https://api.rsbuddy.com/grandExchange?a=graph&g=30&start={start}&i={item_id}'
-    # Retry downloading the item, but with an exponentially
-    # increasing time frame
-    time.sleep(2 ** (max_retries - retries + 1))
+class Retriever(Thread):
+    def __init__(self, item_ids, timestamp, scraper, max_retries, logger):
+        super(Retriever, self).__init__()
 
-    if retries < 1:
-        # When all attempts have failed, log an error
-        logger.log(logging.ERROR, f'Failed to download item id {item_id} after {retries} attempts. Please check the API to find the error or try again another time')
-        return
+        self.item_ids = item_ids
+        # It is called "timestamp" instead of "start",
+        # because start is a Thread-reserved function
+        self.timestamp = timestamp
+        self.scraper = scraper
+        self.max_retries = max_retries
+        self.logger = logger
 
-    try:
-        data = scrape(scraper, url)
-        write_file(f'.data/{item_id}.json', json.dumps(data))
+    def retrieve(self, item_id, retries):
+        if retries < 1:
+            # When all attempts have failed, log an error
+            self.logger.critical(f'Failed to download item id {item_id} after {self.max_retries} attempts. Please check the API to find the error or try again another time.')
+            return
 
-        # Log when an item has been succesfully processed
-        logging.log(logging.INFO, f'Successfully retrieved item id {item_id}\'s price history')
-    except ValueError:
-        # When the downloading has not succeeded, try again
-        retrieve(item_id, start, scraper, max_retries, retries - 1, logger)
+        # Time out the downloading for an exponential
+        # increasing amount of seconds before trying to
+        # download the data again
+        time.sleep(2 ** (self.max_retries - retries) - 1)
 
-def retriever(item_ids, start, scraper, max_retries, logger):
-    '''
-    Function meant as an asynchronous getter for the RSBuddy
-    API JSON data. It needs a list of item ids which it will
-    fetch, a timestamp (in milliseconds) in the past,
-    indicating a timespan from now until the timestamp, and
-    the scraper instance which it will call.
-    '''
-    for item_id in item_ids:
-        retrieve(item_id, start, scraper, max_retries, max_retries, logger)
+        url = f'https://api.rsbuddy.com/grandExchange?a=graph&g=30&start={self.timestamp}&i={item_id}'
+        response = scrape(self.scraper, url)
+
+        try:
+            content = json.loads(response)
+            write_file(f'.data/{item_id}.json', json.dumps(content))
+
+            # Log when an item has been succesfully processed
+            self.logger.info(f'Successfully retrieved item id {item_id}\'s price history.')
+        # A JSONDecodeError is a child of ValueError
+        # TODO: Add JSONDecodeError for clarity
+        except ValueError:
+            self.logger.info(f'Attempt to retrieve item id {item_id} failed. Retrying.')
+
+            # When the downloading has not succeeded, try
+            # again
+            self.retrieve(item_id, retries - 1)
+
+    def run(self):
+        for item_id in self.item_ids:
+            self.retrieve(item_id, self.max_retries)
