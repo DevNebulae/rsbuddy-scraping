c2189f016c0f49b78e3a3fec9b73d087f15dc21b
DevNebulae <gamerkeeperson+github@gmail.com>
2017-10-05 18:10:54 +0200

    Update reducer module to insert documents into a MongoDB database

--- reducer.py
+++ reducer.py
@@ -1,15 +1,23 @@
 from functools import reduce
-from fs_utils import read_json
+from fsutils.json import read_json
 from threading import Thread
+import os
 
 class Reducer(Thread):
-    def __init__(self, items):
+    def __init__(self, database, collection_name, indexes, items):
         super(Reducer, self).__init__()
 
+        self.database = database
+        self.collection_name = collection_name
+        self.indexes = indexes
         self.items = items
 
-    def reduce_items(self, accumulator, item):
-        key, value = item
+    def reduce_items(self, accumulator, index):
+        key, value = self.items[index]
+
+        if not os.path.exists(f'.data/{key}.json'):
+            return accumulator
+
         filename = f'.data/{key}.json'
         data = read_json(filename)
 
@@ -17,5 +25,5 @@ class Reducer(Thread):
         return accumulator
 
     def run(self):
-        items = reduce(self.reduce_items, self.items, [])
-        print(items)
+        items = reduce(self.reduce_items, self.indexes, [])
+        self.database[self.collection_name].insert_many(items)
