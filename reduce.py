a679b4c73ffca06cc43574ac09b2efda6f75a914
DevNebulae <gamerkeeperson+github@gmail.com>
2017-10-05 18:08:51 +0200

    Fix issue in reduce script duplicating records

--- reduce.py
+++ reduce.py
@@ -2,17 +2,35 @@
 import os
 from fsutils.json import read_json
 from pprint import pprint
-from threaded.reducer import Reducer
+from reducer import Reducer
+import numpy as np
+from pymongo import MongoClient
 
 def main():
     if not os.path.exists('.data/items.json'):
         raise Exception('The items.json file in the .data folder does not exist. Please run the scraping script before executing this script.')
 
-    lookup = read_json('.data/items.json')
+    collection_name = 'rsbuddy'
+    client = MongoClient('localhost', 27017)
+    database = client[collection_name]
 
-    reducer = Reducer(list(lookup.items())[:100])
-    reducer.start()
-    reducer.join()
+    items = list(read_json('.data/items.json').items())
+    indexes = np.array_split(np.arange(len(items)), len(items) // 50)
+    threads = []
+    thread_count = 2
+
+    for index in range(len(indexes)):
+        indexes_ = indexes[index]
+
+        thread = Reducer(database, collection_name, indexes_, items)
+        thread.start()
+        threads.append(thread)
+
+        if (index % thread_count == 0 and index != 0):
+            for thread in threads:
+                thread.join()
+
+            threads = []
 
 if __name__ == "__main__":
     main()
